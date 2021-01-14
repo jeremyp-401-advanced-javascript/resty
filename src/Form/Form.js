5a311451fc354ba4669a503e18fcb50e048f5cbb
Jeremy Penning <4085868+pixeljava@users.noreply.github.com>
2021-01-13 21:13:42 -0800

    Changes to add POST/PUT and DELETE functionality.

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -11,7 +11,8 @@ class Form extends React.Component {
       url: ``,
       method: ``,
       result: {},
-      headers: {}
+      headers: {},
+      requestBody: {}
     };
   }
 
@@ -25,35 +26,58 @@ class Form extends React.Component {
     this.setState({ method: newMethod });
   }
 
+  handleRequestBody = (event) => {
+    let newRequestBody = event.target.value;
+    this.setState({ requestBody: newRequestBody });
+  }
+
   prepResults = e => {
-      e.preventDefault();
-      this.getResults();
-      this.setState({ displayResults: true })
+    e.preventDefault();
+    this.getResults();
+    this.setState({ displayResults: true })
   }
 
   getResults = async (e) => {
     const url = this.state.url;
     const method = this.state.method;
+    let requestOptions;
+    let requestHeaders = { "content-type": "application/json; charset=UTF-8" }
+    let responseHeaders = {};
 
-    let headers = {};
+    let requestBody = this.state.requestBody;
 
-    const result = await fetch(url, { method: method, mode: 'cors' })
+    switch (method) {
+      case 'GET':
+        requestOptions = { method: method, mode: 'cors' };
+        break;
+      case 'POST':
+        requestOptions = { method: method, headers: requestHeaders, body: requestBody, mode: 'cors' };
+        break;
+      case 'PUT':
+        requestOptions = { method: method, headers: requestHeaders, body: requestBody, mode: 'cors' };
+        break;
+      case 'DELETE':
+        requestOptions = { method: method, mode: 'cors' };
+        break;
+      default:
+        requestOptions = {};
+        break;
+    }
+
+    const result = await fetch(url, requestOptions)
       .then(response => {
         if (response.status === 200) {
           for (var pair of response.headers.entries()) {
-            headers[pair[0]] = pair[1];
+            responseHeaders[pair[0]] = pair[1];
           }
           return response.json();
         } else {
           return;
         }
       });
-    let resultObj = {};
-    resultObj.count = Object.keys(result).length;
-    resultObj.result = result;
+    this.setState({ result: result });
+    this.setState({ headers: responseHeaders });
 
-    this.setState({ result: resultObj });
-    this.setState({ headers: headers });
   }
 
   render() {
@@ -64,33 +88,41 @@ class Form extends React.Component {
             <legend>URL:</legend>
             <label for="restyUrl">
               <input type="text" id="restyUrlInput" name="restyUrl"
-              placeholder="Enter a URL..." autoFocus onBlur={ this.handleUrl }></input>
-              <button onClick={ this.prepResults } id='submitButton'>Go!</button>
+                placeholder="Enter a URL..." autoFocus onBlur={this.handleUrl}></input>
+              <button onClick={this.prepResults} id='submitButton'>Go!</button>
             </label>
           </fieldset>
-          <fieldset id='restyMethodFields' onChange={ this.handleMethod }>
+          <fieldset id='restyMethodFields' onChange={this.handleMethod}>
             <legend>Choose a method:</legend>
-              <label for="restyGet">
-                <input type="radio" id="restyGet" name="restyMethod" value="GET"></input>
-                <span>GET</span>
-                {/* For a GET I don't think I need to do anything special. */}
-              </label>
-              <label for="restyPost">
-                <input type="radio" id="restyPost" name="restyMethod" value="POST"></input>
-                <span>POST</span>
-                {/* For a POST I need to show an input box to add in some JSON to send with my request */}
-              </label>
-              <label for="restyPut">
-                <input type="radio" id="restyPut" name="restyMethod" value="PUT"></input>
-                <span>PUT</span>
-                {/* For a PUT I need to show an input box to add in some JSON to send with my request */}
-              </label>
-              <label for="restyDelete">
-                <input type="radio" id="restyDelete" name="restyMethod" value="DELETE"></input>
-                <span>DELETE</span>
-                {/* For a DELETE I don't think I need to do anything special. */}
-              </label>
+            <label for="restyGet">
+              <input type="radio" id="restyGet" name="restyMethod" value="GET"></input>
+              <span>GET</span>
+              {/* For a GET I don't think I need to do anything special. */}
+            </label>
+            <label for="restyPost">
+              <input type="radio" id="restyPost" name="restyMethod" value="POST"></input>
+              <span>POST</span>
+              {/* For a POST I need to show an input box to add in some JSON to send with my request */}
+            </label>
+            <label for="restyPut">
+              <input type="radio" id="restyPut" name="restyMethod" value="PUT"></input>
+              <span>PUT</span>
+              {/* For a PUT I need to show an input box to add in some JSON to send with my request */}
+            </label>
+            <label for="restyDelete">
+              <input type="radio" id="restyDelete" name="restyMethod" value="DELETE"></input>
+              <span>DELETE</span>
+              {/* For a DELETE I don't think I need to do anything special. */}
+            </label>
           </fieldset>
+          {(this.state.method === 'POST' || this.state.method === 'PUT') ?
+            <fieldset id='restyRequestBody'>
+              <legend>Request Body:</legend>
+              <textarea name='requestBody' onBlur={this.handleRequestBody}></textarea>
+            </fieldset>
+            : ""
+          }
+
           {!this.state.displayResults ? '' :
             <Results
               url={this.state.url}
